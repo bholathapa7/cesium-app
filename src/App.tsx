bf87a54e75e10350132adb14042936162a761c7b
reube <bhola.thapa@angelswing.io>
2022-09-14 09:38:25 +0545

    add cesium

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ declare global {
 
 // The URL on your server where CesiumJS's static files are hosted.
 window.CESIUM_BASE_URL = '/';
-// Cesium.Ion.defaultAccessToken =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMzBjZDM1Yy04OGVmLTRjNzEtYTYzMS1kYjdjMGM4OWJmNzAiLCJpZCI6MTA3Njk5LCJpYXQiOjE2NjI5NTk1NzR9.gL4j3GgzbkQPW68nYV49_vVFFOBNxnGd1jX7b9-ioO0';
+Cesium.Ion.defaultAccessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyMzBjZDM1Yy04OGVmLTRjNzEtYTYzMS1kYjdjMGM4OWJmNzAiLCJpZCI6MTA3Njk5LCJpYXQiOjE2NjI5NTk1NzR9.gL4j3GgzbkQPW68nYV49_vVFFOBNxnGd1jX7b9-ioO0';
 
 function App() {
   return (
     <div className='App'>
-      <img src='static/logo.svg' />
-      {/* <CesiumContent /> */}
+      {/* <img src='static/logo.svg' /> */}
+      <CesiumContent />
     </div>
   );
 }
--- src/Pages/Cesium.tsx
+++ src/Pages/Cesium.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as Cesium from 'cesium';
 
 const CesiumContent = () => {
-  return <div>CesiumContent</div>;
+  useEffect(() => {
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+      terrainProvider: Cesium.createWorldTerrain()
+    });
+  }, []);
+
+  return <div id='cesiumContainer'></div>;
 };
 
 export default CesiumContent;
